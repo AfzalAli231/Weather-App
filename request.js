a8d513ae860564f0ad1ea2474487204341ea9372
AfzalAli231 <72311983+AfzalAli231@users.noreply.github.com>
2021-04-05 15:52:25 +0500

    Update request.js

--- request.js
+++ request.js
@@ -1,22 +1,22 @@
 const key = '13d24802db392d2fa00515de1f296743';
 
-const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=${city}&appid=${key}';
+// const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=${city}&appid=${key}';
 
-fetch(baseURL)
-    .then((data) => { console.log('response', data.json()) })
-    .catch((error) => {
-        console.log(error);
-    });
+// fetch(baseURL)
+//     .then((data) => { console.log('response', data.json()) })
+//     .catch((error) => {
+//         console.log(error);
+//     });
 
-// const requestCity = async (city) => {
-//     const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
-//     const query = "?q=${city}&appid=${key}";
+const requestCity = async (city) => {
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&appid=${key}`;
 
-//     //make fetch call (promise call)
-//     const response = await fetch(baseURL + query);
+    //make fetch call (promise call)
+    const response = await fetch(baseURL + query);
 
-//     //promise data
-//     const data = await response.json();
-//     return data;
+    //promise data
+    const data = await response.json();
+    return data;
 
-// }
+}
