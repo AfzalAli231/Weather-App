0a997bbec90542e610790b9fdd53d41781c2f5b6
AfzalAli231 <72311983+AfzalAli231@users.noreply.github.com>
2021-04-05 15:40:11 +0500

    Add files via upload

--- request.js
+++ request.js
@@ -1,7 +1,15 @@
-const key = 'LNARzKeZMGf91q98uuCeeGRyQ8dSGJVE';
+const key = '13d24802db392d2fa00515de1f296743';
+
+// const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=cbe3dd267a18f6c89943b3eff94f1ed7';
+
+// fetch(baseURL)
+//     .then((data) => { console.log('response', data.json()) })
+//     .catch((error) => {
+//         console.log(error);
+//     });
 
 const requestCity = async (city) => {
-    const baseURL = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
     const query = `?q=${city}&appid=${key}`;
 
     //make fetch call (promise call)
