cd31e32800337d4a1f87066a05ca3e79b2fb150e
AfzalAli231 <72311983+AfzalAli231@users.noreply.github.com>
2021-04-05 17:44:32 +0500

    Add files via upload

--- request.js
+++ request.js
@@ -1,6 +1,6 @@
 const key = '13d24802db392d2fa00515de1f296743';
 
-// const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=${city}&appid=${key}';
+// const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=Karachi&appid=13d24802db392d2fa00515de1f296743';
 
 // fetch(baseURL)
 //     .then((data) => { console.log('response', data.json()) })
