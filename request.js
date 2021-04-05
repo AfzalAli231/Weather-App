12270e1fea3104a15f400e27ceadf23d88278c68
AfzalAli231 <72311983+AfzalAli231@users.noreply.github.com>
2021-04-05 15:10:13 +0500

    Update request.js

--- request.js
+++ request.js
@@ -1,7 +1,7 @@
 const key = '13d24802db392d2fa00515de1f296743';
 
 const requestCity = async (city) => {
-    const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+    const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
     const query = `?q=${city}&appid=${key}`;
 
     //make fetch call (promise call)
